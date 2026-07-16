e6d806eda4599366b59e0a9ea411c911197fc930
pattaradanai jamprateep <yout3054@gmail.com>
2026-07-16 16:41:05 +0700

    [fsfef] fese

---
+++ workB.py
@@ -0,0 +1,8 @@
+kapok = 0
+noot = 0
+while kapok<50000 :
+    monny = int(input(""))
+    kapok += monny
+    noot += 1
+print(noot)
+print(kapok)
---
+++ workC.py
@@ -0,0 +1,12 @@
+shop = 0
+monny = 0
+while True:
+    i = 0
+    coin = input(f"เก็บร้านที่ {i}")
+    if monny == "police"
+    print ("police")
+    break
+    monny += int(monny)
+print(i)
+
+    
